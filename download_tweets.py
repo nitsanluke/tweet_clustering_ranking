37ef6b605151fabf95a73ca5dc25ad147287a42a
Luke Nitish Kumar <nitsanluke@gmail.com>
2017-04-28 14:01:26 -0400

    Update download_tweets.py

--- download_tweets.py
+++ download_tweets.py
@@ -3,16 +3,16 @@ import json
 import sys
 
 
-CONSUMER_KEY = 'kCTatb4tsfq5lihQAmHhiKkjO'
-CONSUMER_SECRET = 'K5gx7jzQj7KcgK4sYLdgCANM4ywk7jOhmMGe3kvTX5HYm1W2Ed'
-ACCESS_TOKEN = '158395360-BhUmfsA3swXtZAaxAKk8XNqEbXsdek4JRvgvZHDh'
-ACCESS_SECRET = 'vDUE1cCL1n7BLrRKYlGvwQHeKwbF1YX6IrxpkjTs6XQ4l'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
 
 TOTAL_DOWNLOAD = 100000
 BUFFER = 1000
 
 TWEETER_FEEDS = [
-	#'nytimes', 'thesun', 'thetimes', 'ap', 'cnn',
+	'nytimes', 'thesun', 'thetimes', 'ap', 'cnn',
 	'bbcnews', 'cnet', 'msnuk', 'telegraph']
 #				  usatoday, wsj, washingtonpost, bostonglobe, newscomauhq, skynews, sfgate,##
 #				  ajenglish, independent, guardian, latimes, reutersagency, abc, business, bw, time,]
